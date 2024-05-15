5a59e8f112e2ab63717faa3a3b93206e6e52b617
Developer <developer01@volobot.com>
2024-05-16 01:10:09 +0530

    First Commit

---
+++ common/message.py
@@ -0,0 +1,17 @@
+# Success/Failed Messages
+SUCCESS = "Success"
+FAILED = "Failed"
+
+# CUD Methods Messages
+CREATED = "Created successfully"
+UPDATED = "Updated successfully"
+DELETED = "Deleted successfully"
+
+# Read Method Messages
+DATA_EXISTS = "Data already exists"
+DATA_FETCHED = "Data fetched successfully"
+
+# User Messages
+USER_REGISTERED = "User registered successfully"
+USER_LOGIN_SUCCESS = "User logged in successfully"
+USER_LOGOUT_SUCCESS = "User logged out successfully"
