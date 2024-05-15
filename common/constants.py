2db01581c96579e09025cc4475641f03405cb010
Developer <developer01@volobot.com>
2024-05-16 01:40:22 +0530

    Initial Commits - Project Structure/Files

---
+++ common/constants.py
@@ -0,0 +1,11 @@
+EMAIL_REGEX = r"^([\w\.\+\-]+\@[a-zA-Z0-9\.\_]+\.[a-zA-z]{2,4})$"
+
+MOBILE_REGEX = r"^[\d]{10}$"
+
+RATING_RANGE = list(range(1, 11))
+
+IMAGE_TYPES = ["image/bmp", "image/gif", "image/vnd.microsoft.icon",
+               "image/jpeg", "image/png", "image/svg+xml", "image/tiff",
+               "image/webp"]
+
+IMAGE_TYPE_EXTENSIONS = ["bmp","gif","ico","jpg","jpeg","png","svg","tiff","tif","webp"]
---
+++ scripts/sample.py
@@ -0,0 +1,12 @@
+def run():
+    """
+    Write the logic for the script below.
+    No need of return statement.
+    
+    Need to run below command in the terminal
+    --> python manage.py runscript sample
+    """
+
+
+if __name__ == "__main__":
+    run()
